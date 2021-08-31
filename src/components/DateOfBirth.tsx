c033c04b348a8c4e1c2c357d86d54c138ef338b2
Victor Aiyeola <aiyeolavictor@gmail.com>
2021-08-31 10:27:38 +0100

    feat: dob field component

--- src/components/DateOfBirth.tsx
+++ src/components/DateOfBirth.tsx
@@ -3,6 +3,7 @@ import DateFnsUtils from '@date-io/date-fns';
 import Button from '@material-ui/core/Button';
 import FormControl from '@material-ui/core/FormControl';
 import FormHelperText from '@material-ui/core/FormHelperText';
+import Typography from '@material-ui/core/Typography';
 
 export default function DateOfBirth({
   dob,
@@ -15,6 +16,10 @@ export default function DateOfBirth({
 
   return (
     <>
+      <Typography variant="h5" paragraph>
+        How about we get you a birthday present! 🎁😉
+      </Typography>
+
       <FormControl
         error={errors.dob !== undefined}
         style={{
